69db91b31722c4917f6319057f684df88d65a2b2
Tensbeam <tensrrt123@gmail.com>
2022-10-20 14:03:09 +0700

    update

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main() {
+
+    kDataType()
+
+    println("#Any Type \n ==============")
+    val kAny1: Any = 20
+    val kAny2: Any = 100.4
+    val kAny3: Any = "Mark Zuckerberg"
+    println("kAny1: $kAny1")
+    println("kAny2: $kAny2")
+    println("kAny3: $kAny3")
+
+    //    val a = 10 ; val b = 5 ; val c = a+b
+//    println(c)
+//    println("Hello Kotlin!")
+//    println("สวัสดีชาวคอทลิน!")
+//    println(5+5)
+//    println("Hello PaPa" + "What are you doing?")
+//    print(" ทำไรคับ \n")
+//    print("\t Enjoy!")
+}
